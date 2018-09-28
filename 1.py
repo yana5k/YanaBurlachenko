1020f214a46b5a159a56cdf2b0cf0f3c87a7ba00
yana5k <burlachenkoyana@gmail.com>
2018-09-28 10:31:54 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,4 @@
+a = float (input ("Введіть a"))
+b = float (input ("Введіть b"))
+c = float (input ("Введіть c"))
+print (a+b+c)
---
+++ 3.py
@@ -0,0 +1,2 @@
+name = input("Input your name")
+print ("Hello,", name + "!")
